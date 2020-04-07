0b2b80b28bf8bbb207a9fa703a8adce524b3d3d8
eva kramer <eva.kramer91@gmail.com>
2020-04-07 15:37:19 +0100

    created all controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
+  resources :itineraries
+  resources :destinations
+  resources :accommodations
+  resources :activities
+  resources :transports
 end

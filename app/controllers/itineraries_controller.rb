8ca17770c41e5488621784a66b0978fb4ed32531
eva kramer <eva.kramer91@gmail.com>
2020-04-07 16:02:34 +0100

    added actions to itinerary controller

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,2 +1,43 @@
 class ItinerariesController < ApplicationController
+
+    def index
+        @itineraries = Itinerary.all
+    end
+
+    def show
+        @itinerary = Itinerary.find(params[:id])
+    end
+
+    def new
+        @itinerary = Itinerary.new
+    end
+
+    def create
+        @itinerary = Itinerary.new(itinerary_params)
+       if @itinerary.save
+         redirect_to itinerary_path(@itinerary)
+       else
+         render new
+       end
+    end
+
+    def edit
+        @itinerary = Itinerary.find(params[:id])
+    end
+
+    def update
+        @itinerary = Itinerary.find(params[:id])
+        @itinerary.update(itinerary_params)
+        if @itinerary.save
+          redirect_to itinerary_path(@itinerary)
+        else
+          render edit
+        end
+    end
+
+    private
+
+    def itinerary_params
+        params.require(:itinerary).permit(:name, :duration)
+    end
 end

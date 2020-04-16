22dc0c767b89e05744def1aea7fa4cd0e9ac900c
eva kramer <eva.kramer91@gmail.com>
2020-04-16 15:59:41 +0100

    added transport form

--- app/controllers/transports_controller.rb
+++ app/controllers/transports_controller.rb
@@ -1,2 +1,28 @@
 class TransportsController < ApplicationController
+    def new
+        @destination = Destination.find(params[:destination_id])
+        @itinerary = Itinerary.find(params[:itinerary_id])
+        @transport = Transport.new
+    end
+    
+    def create
+        @transport = Transport.new(transport_params)
+        @destination = Destination.find(params[:destination_id])
+        @itinerary = Itinerary.find(params[:itinerary_id])
+        @transport.destination = @destination
+        @destination.itinerary = @itinerary
+        if @transport.save
+            redirect_to itinerary_path(@itinerary)
+        else
+            render :new
+        end
+    end
+
+
+    private
+    
+    def transport_params
+        params.require(:transport).permit(:type, :cost, :start_location, :via_location, :end_location)
+    end
+
 end

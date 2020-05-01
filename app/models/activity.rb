1a6f0d6adfe4d519757927e838f6f6b18437115b
eva kramer <eva.kramer91@gmail.com>
2020-05-01 17:27:28 +0100

    fixed description change form

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,6 @@
 class Activity < ApplicationRecord
     belongs_to :destination
     has_many_attached :photos
-    validates :name, presence: true
-    validates :description, presence: true
+    # validates :name, presence: true
+    # validates :description, presence: true
 end
--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -6,10 +6,10 @@ class Itinerary < ApplicationRecord
   has_many :transports, through: :destinations
   has_many_attached :photos
   #accepts_nested_attributes_for :destinations, reject_if: :all_blank, allow_destroy: true
-  validates :name, presence: true
-  validates :duration, presence: true, numericality: { greater_than: 0 }
-  validates :summary, presence: true
-  validates :photos, presence: true
+  # validates :name, presence: true
+  # validates :duration, presence: true, numericality: { greater_than: 0 }
+  # validates :summary, presence: true
+  # validates :photos, presence: true
  
 
   validates_length_of :name, :maximum => 60

e9155c656cce522eda1881a4604aac8d1040cd1b
eva kramer <eva.kramer91@gmail.com>
2020-06-29 18:48:59 +0100

    color star filled gold

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -3,12 +3,12 @@ class FavouritesController < ApplicationController
     def create
         @itinerary = Itinerary.find(params[:itinerary_id])
         @favourite = Favourite.create(user: current_user, itinerary: @itinerary)
-        redirect_back fallback_location: itineraries_path#(anchor: "favourite-#{@favourite.id}")
+        redirect_back fallback_location: itineraries_path(anchor: "favourite-#{@favourite.id}")
     end
     
     def destroy
         @favourite = Favourite.find(params[:id])
         @favourite.destroy
-        redirect_back fallback_location: itineraries_path#(anchor: "favourite-#{@favourite.id}")
+        redirect_back fallback_location: itineraries_path(anchor: "favourite-#{@favourite.id}")
     end
 end

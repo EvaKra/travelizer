d4077875364061176ee79bdd42f5bc01a5724c70
EvaKra <eva.kramer91@gmail.com>
2020-09-03 16:39:03 +0100

    validation accommodation

--- app/models/accommodation.rb
+++ app/models/accommodation.rb
@@ -1,4 +1,8 @@
 class Accommodation < ApplicationRecord
     belongs_to :destination
     has_many_attached :photos
+
+    validates :name, presence: true
+    validates :accommodation_type, presence: true
+    
 end

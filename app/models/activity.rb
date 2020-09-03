5945d4a1821889dc3db5afb62b0d46ba847676c8
EvaKra <eva.kramer91@gmail.com>
2020-09-03 16:26:34 +0100

    validation activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,7 @@
 class Activity < ApplicationRecord
     belongs_to :destination
     has_many_attached :photos
-    # validates :name, presence: true
-    # validates :description, presence: true
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :cost, presence: true
 end

b4b30154d24a100bf139e0a290d64c8b826753fd
EvaKra <eva.kramer91@gmail.com>
2020-09-03 16:32:22 +0100

    transport validation

--- app/models/transport.rb
+++ app/models/transport.rb
@@ -1,8 +1,8 @@
 class Transport < ApplicationRecord
     belongs_to :destination
     has_many_attached :photos
-    # validates :transport_type, presence: true
-    # validates :description, presence: true
-    # validates :start_location, presence: true
-    # validates :end_location, presence: true
+    validates :transport_type, presence: true
+    validates :description, presence: true
+    validates :start_location, presence: true
+    validates :end_location, presence: true
 end
